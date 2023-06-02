0ea2ddab3fcbd83e95643f7023e4cafd11bc70ac
jubrealguy <whizkidjubrealguy@gmail.com>
2023-06-02 22:51:33 +0100

    tetris

--- Season_2/Tetris/my_tetris.js
+++ Season_2/Tetris/my_tetris.js
@@ -1,6 +1,6 @@
 // This function generates a random integer between the range of [min,max]
 const generateRandomInt = (min, max) => {
-    min = Math.ceil(min);
+    min = Math.round(min);
     max = Math.floor(max);
   
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -23,17 +23,17 @@ const generateRandomTetromino = () => {
       generateRandomTetromino();
     }
   
-    const na = tetrominoSequence.pop();
-    const matrix = tetrominos[na];
+    const name = tetrominoSequence.pop();
+    const matrix = tetrominos[name];
   
     // I and O start centered, all others start in left-middle
     const col = playfield[0].length / 2 - Math.ceil(matrix[0].length / 2);
   
     // I starts on row 21 (-1), all others start on row 22 (-2)
-    const row = na === 'I' ? -1 : -2;
+    const row = name === 'I' ? -1 : -2;
   
     return {
-      name: na,      // name of the piece (L, O, etc.)
+      name: name,      // name of the piece (L, O, etc.)
       matrix: matrix,  // the current rotation matrix
       row: row,        // current row (starts offscreen)
       col: col         // current col
