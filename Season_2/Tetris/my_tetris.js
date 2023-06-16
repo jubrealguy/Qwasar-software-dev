d301b2bc56d162faae5cfd7e8148e99b81b69fbb
jubrealguy <whizkidjubrealguy@gmail.com>
2023-06-16 11:39:38 +0100

    completed the tetris

--- Season_2/Tetris/my_tetris.js
+++ Season_2/Tetris/my_tetris.js
@@ -1,14 +1,14 @@
-// This function generates a random integer between the range of [min,max]
-const generateRandomInt = (min, max) => {
-    min = Math.round(min);
-    max = Math.floor(max);
-  
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+// Generating a random integer between the range of min and max after rounding up to integer
+function generateRandomInt(min, max) {
+  const intMin = Math.ceil(min);
+  const intMax = Math.floor(max);
+
+  return Math.floor(Math.random() * (intMax - intMin + 1)) + intMin;
+}
   
   // This function generates a new tetromino sequence
 const generateRandomTetromino = () => {
-    const tetrominosequence = ['I', 'J', 'L', 'O', 'S', 'T', 'Z'];
+    const tetrominosequence = ['I', 'O', 'T', 'S', 'Z', 'J', 'L'];
   
     while (tetrominosequence.length) {
       const randIndex = generateRandomInt(0, tetrominosequence.length - 1);
@@ -18,51 +18,57 @@ const generateRandomTetromino = () => {
   }
   
   // This function generates a random tetromino sequence which will be the next
-  function getNextTetromino() {
+  function nextTetromino() {
+    //call the function generateRandomTetromino when array is empty
     if (tetrominoSequence.length === 0) {
       generateRandomTetromino();
     }
   
     const name = tetrominoSequence.pop();
+
     const matrix = tetrominos[name];
   
-    // I and O start centered, all others start in left-middle
-    const col = playfield[0].length / 2 - Math.ceil(matrix[0].length / 2);
+    const column = Math.floor((playfield[0].length - matrix[0].length) / 2);
   
-    // I starts on row 21 (-1), all others start on row 22 (-2)
     const row = name === 'I' ? -1 : -2;
   
+    // return the name of piece, current rotation matrix, current row and current col
     return {
-      name: name,      // name of the piece (L, O, etc.)
-      matrix: matrix,  // the current rotation matrix
-      row: row,        // current row (starts offscreen)
-      col: col         // current col
+      name: name,
+      matrix: matrix,
+      row: row,
+      col: column        
     };
   }
   
-  // rotate an NxN matrix 90deg
-  // @see https://codereview.stackexchange.com/a/186834
+  // rotating a square matrix 90degrees clockwise and returning the resultingmatrix
   function rotate(matrix) {
-    const N = matrix.length - 1;
-    const result = matrix.map((row, i) =>
-      row.map((val, j) => matrix[N - j][i])
-    );
+    const N = matrix.length;
+    const resultingMatrix = [];
   
-    return result;
+    for (let i = 0; i < N; i++) {
+      const newRow = [];
+      for (let j = 0; j < N; j++) {
+        newRow.push(matrix[N - j - 1][i]);
+      }
+      resultingMatrix.push(newRow);
+    }
+  
+    return resultingMatrix;
   }
   
-  // check to see if the new matrix/row/col is valid
-  function isValidMove(matrix, cellRow, cellCol) {
+  // check to see if the new matrix is a valid move within the game
+  function moveValid(matrix, cellRow, cellColumn) {
+    const playfieldHeight = playfield.length;
+    const playfieldWidth = playfield[0].length;
+  
     for (let row = 0; row < matrix.length; row++) {
       for (let col = 0; col < matrix[row].length; col++) {
-        if (matrix[row][col] && (
-            // outside the game bounds
-            cellCol + col < 0 ||
-            cellCol + col >= playfield[0].length ||
-            cellRow + row >= playfield.length ||
-            // collides with another piece
-            playfield[cellRow + row][cellCol + col])
-          ) {
+        if (
+          matrix[row][col] &&
+          (cellColumn + col < 0 || cellColumn + col >= playfieldWidth ||
+            cellRow + row >= playfieldHeight || playfield[cellRow + row][cellColumn + col])
+        ) {
           return false;
         }
       }
@@ -71,56 +77,55 @@ const generateRandomTetromino = () => {
     return true;
   }
   
-  // place the tetromino on the playfield
-  function placeTetromino() {
+  // The positioning and handling of the tetromino pieces in the game
+  function placingTetromino() {
     for (let row = 0; row < tetromino.matrix.length; row++) {
       for (let col = 0; col < tetromino.matrix[row].length; col++) {
         if (tetromino.matrix[row][col]) {
+          const targetRow = tetromino.row + row;
+          const targetCol = tetromino.col + col;
   
-          // game over if piece has any part offscreen
-          if (tetromino.row + row < 0) {
-            return showGameOver();
+          // End Game if any part goes offscreen
+          if (targetRow < 0) {
+            return displayGameOver();
           }
   
-          playfield[tetromino.row + row][tetromino.col + col] = tetromino.name;
+          playfield[targetRow][targetCol] = tetromino.name;
         }
       }
     }
   
-    // check for line clears starting from the bottom and working our way up
-    for (let row = playfield.length - 1; row >= 0; ) {
-      if (playfield[row].every(cell => !!cell)) {
-  
-        // drop every row above this one
+    // Check for line clears
+    for (let row = playfield.length - 1; row >= 0; row--) {
+      if (playfield[row].every((cell) => !!cell)) {
+        
         for (let r = row; r >= 0; r--) {
           for (let c = 0; c < playfield[r].length; c++) {
-            playfield[r][c] = playfield[r-1][c];
+            playfield[r][c] = playfield[r - 1][c];
           }
         }
       }
-      else {
-        row--;
-      }
     }
   
-    tetromino = getNextTetromino();
+    tetromino = nextTetromino();
   }
   
   // show the game over screen
-  function showGameOver() {
-    cancelAnimationFrame(rAF);
+  function displayGameOver() {
+    cancelAnimationFrame(reqAnimatonFrame);
     gameOver = true;
   
-    context.fillStyle = 'black';
-    context.globalAlpha = 0.75;
-    context.fillRect(0, canvas.height / 2 - 30, canvas.width, 60);
+    const rectHeight = 60;
+    const rectY = canvas.height / 2 - rectHeight / 2;
   
-    context.globalAlpha = 1;
+    context.fillStyle = 'rgba(0, 0, 0, 0.75)';
+    context.fillRect(0, rectY, canvas.width, rectHeight);
+  
     context.fillStyle = 'white';
-    context.font = '36px monospace';
+    context.font = '40px monospace';
     context.textAlign = 'center';
     context.textBaseline = 'middle';
-    context.fillText('GAME OVER!', canvas.width / 2, canvas.height / 2);
+    context.fillText('GAME OVER!', canvas.width / 2, canvas.height / 3);
   }
   
   const canvas = document.getElementById('game');
@@ -128,11 +133,9 @@ const generateRandomTetromino = () => {
   const grid = 32;
   const tetrominoSequence = [];
   
-  // keep track of what is in every cell of the game using a 2d array
-  // tetris playfield is 10x20, with a few rows offscreen
+  // A 10 x 20 playfield with some off-field rows
   const playfield = [];
   
-  // populate the empty state
   for (let row = -2; row < 20; row++) {
     playfield[row] = [];
   
@@ -141,8 +144,7 @@ const generateRandomTetromino = () => {
     }
   }
   
-  // how to draw each tetromino
-  // @see https://tetris.fandom.com/wiki/SRS
+  // Drawing each tetromino
   const tetrominos = {
     'I': [
       [0,0,0,0],
@@ -193,94 +195,99 @@ const generateRandomTetromino = () => {
   };
   
   let count = 0;
-  let tetromino = getNextTetromino();
-  let rAF = null;  // keep track of the animation frame so we can cancel it
+  let tetromino = nextTetromino();
+  let reqAnimatonFrame = null;
   let gameOver = false;
   
-  // game loop
+  // Function responsible for the main game, it updates amd renders the state ofthe game repeatedly
   function loop() {
-    rAF = requestAnimationFrame(loop);
-    context.clearRect(0,0,canvas.width,canvas.height);
+    reqAnimatonFrame = requestAnimationFrame(loop);
+    context.clearRect(0, 0, canvas.width, canvas.height);
   
-    // draw the playfield
-    for (let row = 0; row < 20; row++) {
-      for (let col = 0; col < 10; col++) {
-        if (playfield[row][col]) {
-          const name = playfield[row][col];
-          context.fillStyle = colors[name];
-  
-          // drawing 1 px smaller than the grid creates a grid effect
-          context.fillRect(col * grid, row * grid, grid-1, grid-1);
+    // Draw the playfield
+    for (let row = 0; row < playfield.length; row++) {
+      for (let col = 0; col < playfield[row].length; col++) {
+        const cell = playfield[row][col];
+        if (cell) {
+          const color = colors[cell];
+          context.fillStyle = color;
+          context.fillRect(col * grid, row * grid, grid - 1, grid - 1);
         }
       }
     }
   
-    // draw the active tetromino
+    // Draw the active tetromino
     if (tetromino) {
-  
-      // tetromino falls every 35 frames
       if (++count > 35) {
         tetromino.row++;
         count = 0;
   
-        // place piece if it runs into anything
-        if (!isValidMove(tetromino.matrix, tetromino.row, tetromino.col)) {
+        if (!moveValid(tetromino.matrix, tetromino.row, tetromino.col)) {
           tetromino.row--;
-          placeTetromino();
+          placingTetromino();
         }
       }
   
-      context.fillStyle = colors[tetromino.name];
+      const tetrominoColor = colors[tetromino.name];
+      context.fillStyle = tetrominoColor;
   
       for (let row = 0; row < tetromino.matrix.length; row++) {
         for (let col = 0; col < tetromino.matrix[row].length; col++) {
           if (tetromino.matrix[row][col]) {
-  
-            // drawing 1 px smaller than the grid creates a grid effect
-            context.fillRect((tetromino.col + col) * grid, (tetromino.row + row) * grid, grid-1, grid-1);
+            const posX = (tetromino.col + col) * grid;
+            const posY = (tetromino.row + row) * grid;
+            context.fillRect(posX, posY, grid - 1, grid - 1);
           }
         }
       }
     }
   }
   
-  // listen to keyboard events to move the active tetromino
+
+  function moveTetromino(direction) {
+    const col = tetromino.col + direction;
+    if (moveValid(tetromino.matrix, tetromino.row, col)) {
+      tetromino.col = col;
+    }
+  }
+  
+  function rotateTetromino() {
+    const matrix = rotate(tetromino.matrix);
+    if (moveValid(matrix, tetromino.row, tetromino.col)) {
+      tetromino.matrix = matrix;
+    }
+  }
+  
+  function dropTetromino() {
+    const row = tetromino.row + 1;
+    if (!moveValid(tetromino.matrix, row, tetromino.col)) {
+      tetromino.row = row - 1;
+      placeTetromino();
+      return;
+    }
+    tetromino.row = row;
+  }
+  // listening to keyboard events to move the active tetromino
   document.addEventListener('keydown', function(e) {
-    if (gameOver) return;
-  
-    // left and right arrow keys (move)
-    if (e.which === 37 || e.which === 39) {
-      const col = e.which === 37
-        ? tetromino.col - 1
-        : tetromino.col + 1;
-  
-      if (isValidMove(tetromino.matrix, tetromino.row, col)) {
-        tetromino.col = col;
-      }
+    if (gameOver) {
+      return;
     }
   
-    // up arrow key (rotate)
-    if (e.which === 38) {
-      const matrix = rotate(tetromino.matrix);
-      if (isValidMove(matrix, tetromino.row, tetromino.col)) {
-        tetromino.matrix = matrix;
-      }
-    }
-  
-    // down arrow key (drop)
-    if(e.which === 40) {
-      const row = tetromino.row + 1;
-  
-      if (!isValidMove(tetromino.matrix, row, tetromino.col)) {
-        tetromino.row = row - 1;
-  
-        placeTetromino();
-        return;
-      }
-  
-      tetromino.row = row;
+    switch (e.which) {
+      case 37: // Left arrow key (move left)
+        moveTetromino(-1);
+        break;
+      case 39: // Right arrow key (move right)
+        moveTetromino(1);
+        break;
+      case 38: // Up arrow key (rotate)
+        rotateTetromino();
+        break;
+      case 40: // Down arrow key (drop)
+        dropTetromino();
+        break;
     }
   });
   
   // start the game
-  rAF = requestAnimationFrame(loop);
+  reqAnimatonFrame = requestAnimationFrame(loop);
